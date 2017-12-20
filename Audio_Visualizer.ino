5c763be3590f8870d02cc9bc71488c643f9d01c4
Alexander Shi <ayshi@edu.uwaterloo.ca>
2017-12-20 18:53:37 -0500

    use a noise threshold taken from ambient noise peaks

--- Audio_Visualizer.ino
+++ Audio_Visualizer.ino
@@ -6,10 +6,12 @@
 //Alexander Shi
 //last modified 19 July 2015
 #include <Arduino.h>
-#include <Time.h>
+//#include <Time.h>
 #include "WordBuilder.cpp"
 #include "DrawingEngine.cpp"
 
+// external includes
+#include <FHT.h>
 
 //IO    
 #define LEDARRAY_D    11
@@ -24,19 +26,39 @@
 #define MICROPHONE    A0
 
 
-const int INTENSITY_THRESHOLD = 300;
-int curValue = 0;
-int preValue = 0;
+static int INTENSITY_THRESHOLD = 350;
+
+static int curValue = 0;
+static int preValue = 0;
+
 DrawingEngine drawingEngine;
 unsigned char mutableWord[1][32];
 //each index position represents one of 32 columns in a 16 wide, 2 tall array (cables being on the "bottom" side), 
   //values from 0->255 loaded for each column (MSD on bottom, LSD on top)
 unsigned char testWord[1][32]= 
 {
-
-0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x04, 0x09, 0x09, 0x04, 0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 
-0x00, 0x00, 0x00, 0x00, 0x80, 0x40, 0x20, 0x90, 0x90, 0x20, 0x40, 0x80, 0x00, 0x00, 0x00, 0x00
+  0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x04, 0x09, 0x09, 0x04, 0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x80, 0x40, 0x20, 0x90, 0x90, 0x20, 0x40, 0x80, 0x00, 0x00, 0x00, 0x00
 };
+
+unsigned GetSoundThreshold() { 
+    const unsigned TEST_POINT_COUNT = 100;
+    unsigned long long testAggr = 0;
+    unsigned maxVal = 0;
+    unsigned A0_Val = 0;
+  
+    for (int i = 0; i < TEST_POINT_COUNT; ++i) {
+      delay(1); // max analog read speed is about 100 us
+      A0_Val = analogRead(A0);  
+//    testAggr += A0_Val;
+      maxVal = A0_Val > maxVal ? A0_Val : maxVal;
+//    Serial.println(A0_Val);
+    }
+//  INTENSITY_THRESHOLD = testAggr / TEST_POINT_COUNT;
+//  Serial.println(INTENSITY_THRESHOLD);
+    maxVal = maxVal < 200 ? 200 : maxVal;
+    return 2*maxVal > 1023 ? 800 : 2*maxVal;
+}
 
 void setup()
 {
@@ -50,16 +72,15 @@ void setup()
   pinMode(LEDARRAY_LAT, OUTPUT);
   pinMode(BUTTON, INPUT);
   pinMode(MICROPHONE, INPUT);
+  
 //  for (int i=1; i<=16;i++){
 //  drawPixel(mutableWord, i,i);  
 //  }
-//  for (int i = 0; i<15; i++)
 //  WordBuilder::drawCenterDiamond(mutableWord,5);  
 //  WordBuilder::drawPixel(mutableWord,2,2);
 //  WordBuilder::drawPixel(mutableWord,2,15);
 //  WordBuilder::drawPixel(mutableWord,15,2);
 //  WordBuilder::drawPixel(mutableWord,15,15);
-  if (true) {
     WordBuilder::drawLine(mutableWord, 9,9,13,16);
     WordBuilder::drawLine(mutableWord, 8,9,4,16);
     WordBuilder::drawLine(mutableWord, 8,8,4,1);
@@ -68,28 +89,28 @@ void setup()
     WordBuilder::drawLine(mutableWord, 9,8,16,4);
     WordBuilder::drawLine(mutableWord, 8,8,1,4);
     WordBuilder::drawLine(mutableWord, 8,9,1,13);
-    drawingEngine = DrawingEngine();  
-    WordBuilder::cleanWord(testWord);
-  }
-//  Serial.begin(250000);
+  Serial.begin(9600);
+  INTENSITY_THRESHOLD = GetSoundThreshold();
+  Serial.println(INTENSITY_THRESHOLD);
+
+  drawingEngine = DrawingEngine();
+  //    WordBuilder::cleanWord(testWord);  
 }
 
 void loop()
 {
 //  preValue = curValue;
   curValue = analogRead(A0);
-//  Serial.println(curValue);
+  delay(1); // max analog read speed is about 100 us
   char hit = curValue>INTENSITY_THRESHOLD;
   if (!hit){
-    drawingEngine.Display(testWord);
+    drawingEngine.Display(testWord); 
   }
   else{    
       drawingEngine.Display(mutableWord);
-      delay(5);
+      Serial.println(curValue);
+      delay(10);
   }
-//      drawingEngine.Display(mutableWord);
-//      if (millis() > 1000)
-//        WordBuilder::cleanWord(mutableWord);
 }
 
 
